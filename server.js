bb030aa242878d20616d0bba55379222dc18c32f
Mudasirh120 <mudasirh120@gmail.com>
2025-08-17 15:11:50 +0500

    added favicon

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const __dirname = dirname(__filename);
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/url", urlRouter);
